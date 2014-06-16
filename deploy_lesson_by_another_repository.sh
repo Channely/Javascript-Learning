5144af9f1213a91777db34c05c693e02a5ee9001
Channely <qijie29896@gmail.com>
2014-06-16 10:42:37 +0800

    init the javascript - base course project

---
+++ deploy_lesson_by_another_repository.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#get the commit content by user input
+echo "\033[33m Please input the commit words : \033[0m"
+read commit
+echo "\033[34m Content:'$commit' \033[0m"
+
+#push code change on this respo.
+echo "\033[33m now pushing code local \033[0m"
+git add .
+git commit -m "${commit}"
+git pull --rebase
+git push
+echo "\033[32m local end \033[0m"
+
+#sync deploy project to another repos.
+echo "\033[33m sync code \033[0m"
+rm -rf ../Javascript-Learning-Deploy/*
+cp -rf PracticeProject/* ../Javascript-Learning-Deploy/
+cd ../Javascript-Learning-Deploy/
+echo "\033[33m now pushing code another \033[0m"
+git add .
+git commit -m "${commit}"
+git pull --rebase
+git push
+echo "\033[32m another end \033[0m"
+cd ../Javascript-Learning/
+ls
+#end it.
